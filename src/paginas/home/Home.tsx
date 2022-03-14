e3aa9fbcb48d318b83144656a5b2303f6a5f48e1
moggishere <aryyoon@gmail.com>
2022-03-14 19:25:55 -0300

    removido dependencias nao utilizadas

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,13 +1,9 @@
 import React, { useEffect } from 'react';
-import { Typography, Box, Grid, Button, Paper } from '@material-ui/core';
 import './Home.css';
 import { useHistory } from 'react-router';
 import { useSelector } from 'react-redux';
 import { TokenState } from '../../store/tokens/tokensReducer';
-import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
-import ModalTema from '../../components/temas/modalTema/ModalTema';
-import TelaTema from '../../components/temas/telaTema/TelaTema';
 import ListaTema from '../../components/temas/listatema/ListaTema';
 
 
